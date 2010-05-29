ec0c661fc7283df86c1704b1a465e4a00ad177b0
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-05-29 14:16:47 +0400

    Add type synonims for low cardinality numbers

---
+++ Types/Number/Nat/Num.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Types.Number.Nat.Num where
+
+import Types.Number.Nat
+
+type N0 = $(natT 0)
+type N1 = $(natT 1)
+type N2 = $(natT 2)
+type N3 = $(natT 3)
+type N4 = $(natT 4)
+type N5 = $(natT 5)
+type N6 = $(natT 6)
+type N7 = $(natT 7)
+type N8 = $(natT 8)
+type N9 = $(natT 9)
