76c308093dcbc3b0e433801ac3387b995628aa43
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-07-31 15:55:49 +0400

    Add Reify type class for conversion to values

---
+++ TypeLevel/Reify.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+-- |
+-- Module      : TypeLevel.Reify
+-- Copyright   : Alexey Khudyakov
+-- License     : BSD3-style (see LICENSE)
+--
+-- Maintainer  : Alexey Khudyakov <alexey.skladnoy@gmail.com>
+-- Stability   : unstable
+-- Portability : unportable (GHC only)
+module TypeLevel.Reify ( Witness(..)
+                       , Reify(..)
+                       ) where
+
+
+data Witness t a = Witness { getValue :: a }
+
+-- | Convert type level into value level using 
+class Reify t a where
+  witness :: Witness t a
+
+       
+       
